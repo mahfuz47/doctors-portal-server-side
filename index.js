545f562b50235579bdbdc848a263f16406b0bb09
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-19 12:04:57 +0600

    initial setup with mongodb done

---
+++ index.js
@@ -0,0 +1,45 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dsklw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("db connected");
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Doctor!");
+});
+
+app.listen(port, () => {
+  console.log("Example app listening on port", port);
+});
