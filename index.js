438ef7fb1aea444c84a05d658dc2257973651fdb
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-19 13:06:31 +0600

    get booking added

--- index.js
+++ index.js
@@ -23,12 +23,46 @@ async function run() {
     const serviceCollection = client
       .db("doctors_portal")
       .collection("services");
+    const bookingCollection = client
+      .db("doctors_portal")
+      .collection("bookings");
 
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    /**
+     * API Naming Convention
+     * app.get('/booking)// get all bookings in this collection or get more than one or filter
+     * app.get('/booking/:id')// get a specific booking
+     * app.post('/booking')// add a new booking
+     * app.patch('/booking/:id')// update a booking
+     * app.delete('/booking/:id')// delete a booking
+     */
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    });
+
+    app.get("/booking", async (req, res) => {
+      const query = {};
+      const cursor = bookingCollection.find(query);
+      const booking = await cursor.toArray();
+      res.send(booking);
     });
   } finally {
   }
